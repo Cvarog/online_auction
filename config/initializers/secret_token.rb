bdc51edc1a2a1d17861e0e03d3cab4b94e0de0a9
Cvarog <uchihamilord@gmail.com>
2013-12-02 17:20:11 +0400

    Online Auction. First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnlineAuction::Application.config.secret_key_base = 'bd806d38fdd4c1adc490cf84583aff13fc14e560b840ae97f545d4d020d9f17438c725ec1cb673db2955c3743e95e3d9906784a9395b375252924653cd4f1f2c'
